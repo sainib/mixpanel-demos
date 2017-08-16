afbca9132cb23137cfe49cef3a128b0619cafe23
root <root@birens-hdp0.field.hortonworks.com>
2017-08-16 03:01:12 +0000

    direct http python app

---
+++ Python_CLI/app-http-api.py
@@ -0,0 +1,8 @@
+import json, base64, requests
+
+d={"userId": "user1","event": "Product Viewed","properties": {"distinct_id": "user1","product_id": "pr_507f1f77bcf86cd799439011","sku": "G-32","category": "Games","name": "Monopoly: 3rd Edition","brand": "Hasbro","variant": "200 pieces","price": 18.99,"quantity": 1,"token": "83af3c32b1c73c25bf7d5d420444a582"}}
+
+s = json.dumps(d)
+ed = base64.b64encode(s)
+
+r = requests.post(url = 'http://api.mixpanel.com/track/?data=' + ed )
--- Python_CLI/run.sh
+++ Python_CLI/run.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-python app.py
+while true; 
+do
+    read -p "Which python app you want to execute?
+1. The one that uses MixPanel library?
+2. The one that uses direct HTTP API?
+Type 1 or 2..." yn
+
+   case $yn in
+	1) echo "Executing python app to send MP events using MP libs.";python app-mp-lib.py; echo "Done. Exiting.."; exit ;;
+        2) echo "Executing python app to send MP events using direct HTTP API.";python app-http-api.py; echo "Done. Exiting.."; exit ;;
+        *) echo "How hard is it to type just 1 or 2? :-) Lets try again, shall we?";;
+    esac
+done
