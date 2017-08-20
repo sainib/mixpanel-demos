2161ecff2b27df442adb2e86092545413ca4720f
Biren Saini <sainib@Birens-MacBook-Pro.local>
2017-08-20 12:36:04 -0400

    Done with the HTML-PHP Web example

--- HTML_PHP/application/controllers/Welcome.php
+++ HTML_PHP/application/controllers/Welcome.php
@@ -25,7 +25,26 @@ class Welcome extends CI_Controller {
 
 	public function logEvent()
 	{
-		echo "Done";
+		ini_set('display_errors', 1);
+
+		//More elegant way would be to just create an array in PHP and then use json_encode to create a JSON String object.. but copying and pasting from Python example 
+		$d='{"userId": "user1","event": "Product Viewed","properties": {"distinct_id": "user1","product_id": "pr_507f1f77bcf86cd799439011","sku": "G-32","category": "Games","name": "Monopoly: 3rd Edition","brand": "Hasbro","variant": "200 pieces","price": 18.99,"quantity": 1,"token": "83af3c32b1c73c25bf7d5d420444a582"}}';
+		$ed = base64_encode($d);
+
+		$url_send="http://api.mixpanel.com/track/";
+	
+	  $fields_string = "data=".$ed;
+	  $ch = curl_init($url_send);
+	  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");  
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	  curl_setopt($ch, CURLOPT_POSTFIELDS,$fields_string);
+	  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
+	  $result = curl_exec($ch);
+	  curl_close($ch);  // Seems like good practice
+	  echo $result;	
+
 	}
+	
+	
 
 }
